1f7b11123cf2d459ef0434899c2a6a00709f0d58
xobett <cessarr862@gmail.com>
2025-12-17 21:37:57 -0600

    Add base project, db context and main entities

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
