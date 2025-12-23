38e9e432a4973d0134c1d756c1273c83d96ced23
xobett <cessarr862@gmail.com>
2025-12-22 22:12:30 -0600

    Save additional changes

---
+++ Core/Interfaces/ISpecification.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Linq.Expressions;
+
+namespace Core.Interfaces;
+
+public interface ISpecification<T>
+{
+    Expression<Func<T, bool>> Criteria { get; }
+}
---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Infrastructure.Data;
+
+public class SpecificationEvaluator
+{
+
+}
