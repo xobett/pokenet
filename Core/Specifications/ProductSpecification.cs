6fa651ba85aa337d43b80a9c0b034d3269ccb646
xobett <cessarr862@gmail.com>
2025-12-22 23:14:07 -0600

    Add unsaved changes

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -1,8 +1,15 @@
 using System;
+using Core.Entities;
 
 namespace Core.Specifications;
 
-public class ProductSpecification
+public class ProductSpecification : BaseSpecification<Product>
 {
-
+    public ProductSpecification(string? brand, string? type) : base(x =>
+        (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) &&
+        (string.IsNullOrWhiteSpace(type) || x.Type == type)
+    )
+    {
+        
+    }
 }
