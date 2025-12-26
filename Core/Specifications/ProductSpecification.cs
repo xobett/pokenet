fd51fb01970af7948150359abbea7961d55a370a
xobett <cessarr862@gmail.com>
2025-12-25 20:51:46 -0600

    Add base api controller and additional search feature

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -5,12 +5,15 @@ namespace Core.Specifications;
 
 public class ProductSpecification : BaseSpecification<Product>
 {
-    public ProductSpecification(string? brand, string? type, string? sort) : base(x =>
-        (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) &&
-        (string.IsNullOrWhiteSpace(type) || x.Type == type)
+    public ProductSpecification(ProductSpecParams specParams) : base(x =>
+        (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
+        (!specParams.Brands.Any() || specParams.Brands.Contains(x.Brand)) &&
+        (!specParams.Types.Any() || specParams.Types.Contains(x.Type))
     )
     {
-        switch (sort)
+        ApplyPaging(specParams.PageSize * (specParams.PageIndex - 1), specParams.PageSize);
+        
+        switch (specParams.Sort)
         {
             case "priceAsc": AddOrderBy(x => x.Price); break;
             case "priceDesc": AddOrderByDescending(x => x.Price); break;
